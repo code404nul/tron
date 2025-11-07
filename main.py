9361163dedc3aa8865e7158593725200aa5a19ce
arch <enrsalut@gmail.com>
2025-11-07 09:23:20 +0100

    Delete dataclass config -> constant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 # This is done without any external libraries, so colorama is not included. That can cause issues to display colors.
 from os import get_terminal_size
-from dataclasses import dataclass
 
 COLOR  = {
     "black": "\033[31m",
@@ -12,22 +11,21 @@ COLOR  = {
     "reset": "\033[0m"
 }
 
-@dataclass
-class config:
-    size: int = 20
-    factor: float = 1
-    real_size: int = int(size * factor)
+CONFIG_SIZE: int = get_terminal_size().lines - 5
+CONFIG_FACTOR: int = 2
+CONFIG_REAL_SIZE: int = CONFIG_SIZE * CONFIG_FACTOR
 
 class board:
     def __init__(self):
+
         self.border_char = '#'
         self.show_color = "white" #change color here
     
     def show_staduim(self):
-        print(f"{COLOR[self.show_color]}{self.border_char * config.real_size}{COLOR['reset']}")
-        for i in range(config.real_size - config.factor):
-            print(f"{COLOR[self.show_color]}{self.border_char}{COLOR['reset']}{' ' * int((config.size - 2) * config.factor + 2)}{COLOR[self.show_color]}{self.border_char}{COLOR['reset']}")
-        print(f"{COLOR[self.show_color]}{self.border_char * config.real_size}{COLOR['reset']}")
+        print(f"{COLOR[self.show_color]}{self.border_char * CONFIG_REAL_SIZE}{COLOR['reset']}")
+        for i in range(CONFIG_SIZE - 2):
+            print(f"{COLOR[self.show_color]}{self.border_char}{COLOR['reset']}{' ' * ((CONFIG_SIZE - 2) * CONFIG_FACTOR + 2)}{COLOR[self.show_color]}{self.border_char}{COLOR['reset']}")
+        print(f"{COLOR[self.show_color]}{self.border_char * CONFIG_REAL_SIZE}{COLOR['reset']}")
         
 class player:
     def __init__(self, color, name, position):
@@ -38,7 +36,6 @@ class player:
         
     def show_on_screen(self):
         pass
-    
     
 area = board()
 area.show_staduim()
