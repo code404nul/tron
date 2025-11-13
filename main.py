ffc31187cb4483322d8b6ae3486274ad3b76f500
arch <enrsalut@gmail.com>
2025-11-13 11:21:28 +0100

    News board + collistion joueur

--- main.py
+++ main.py
@@ -71,27 +71,37 @@ class Board:
             self.board.append(("#", "white"))
         self.board += [("#", "white")] * CONFIG_REAL_SIZE
     
+    def _check_collision(self):
+        pos_collistion = []
+        for player in self.players:
+            for position in pos_collistion:
+                if position == player.get_position():
+                    return True
+            pos_collistion.append(player.get_position())
+        return False
+
     def add_player(self, player): self.players.append(player)
     
     def show_stadium(self):
         system("clear")
         
+        if self._check_collision():
+            self.game_over()
+            return
+
         for case in range(len(self.board)):
-            player_at_pos = None
+            char, color = self.board[case]
             for player in self.players:
                 if case == player.get_position():
-                    player_at_pos = player
+                    char, color = player.symbol, player.color
                     break
-            
-            if player_at_pos:
-                print(player_at_pos.render(), end="" if (case + 1) % CONFIG_REAL_SIZE != 0 else "\n", flush=True)
+                
+            if (case + 1) % CONFIG_REAL_SIZE == 0:
+                print(f"{COLOR[color]}{char}{COLOR['reset']}")
             else:
-                char, color = self.board[case]
-                if (case + 1) % CONFIG_REAL_SIZE == 0:
-                    print(f"{COLOR[color]}{char}{COLOR['reset']}")
-                else:
-                    print(f"{COLOR[color]}{char}{COLOR['reset']}", end="", flush=True)
+                print(f"{COLOR[color]}{char}{COLOR['reset']}", end="", flush=True)
                     
+
     def game_over(self):
         system("clear")
         print(f"{COLOR['white']}{self.GAME_OVER_SCREEN}{COLOR['reset']}")
@@ -105,12 +115,9 @@ board_instance.add_player(player_orange)
 board_instance.show_stadium()
 
 def test():
-    player_blue.move_down()
-    player_orange.move_up()
+    player_orange.move_left()
     board_instance.show_stadium()
     
-for i in range(40):
-    sleep(0.03)
+for i in range(16):
+    sleep(0.01)
     test()
-    
-board_instance.game_over()
