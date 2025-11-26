8332798934c75cca7e66bcf2f49bc6dda18b5e81
arch <enrsalut@gmail.com>
2025-11-26 13:57:42 +0100

    nn

--- nn.py
+++ nn.py
@@ -1,25 +1,33 @@
-from math import log2, exp
+from math import log2, exp, sqrt
 from random import uniform, gauss, randint
 from copy import deepcopy
 from time import sleep
 from os import system
 
+
 class Neuron:
-    def __init__(self, next_layer_size):
-        self.bias = uniform(-1, 1)
-        self.a = uniform(-1, 1)
-        self.weights = [uniform(-1, 1) for _ in range(next_layer_size)]
-
-    def sigmoid(self, x): return 1/(1+exp(-x*1.5)) #Le 1.5 c'est la sauce du chef, c'est pour accepter -2 a 2 en input, ca fait chelou de retoucher a geogebra
-
+    def __init__(self, input_size):
+        # Initialisation Xavier/He https://www.geeksforgeeks.org/deep-learning/xavier-initialization/
+        limit = sqrt(2.0 / input_size)
+        print(input_size)
+        self.bias = uniform(-limit, limit)
+        self.a = 0.0
+        self.weights = [gauss(0, limit) for _ in range(input_size)]
+        print(self.weights)
+    
+    def sigmoid(self, x):
+        x_clamped = max(-20, min(20, x))
+        if x_clamped >= 0:
+            return 1 / (1 + exp(-x_clamped))
+        else:
+            exp_x = exp(x_clamped)
+            return exp_x / (1 + exp_x)
+    
     def activation(self, inputs):
-        weighted_sum = 0
-        for input_value, weight in zip(inputs, self.weights):
-            weighted_sum += input_value * weight
-
-        weighted_sum += self.bias
+        weighted_sum = sum(inp * w for inp, w in zip(inputs, self.weights)) + self.bias
         self.a = self.sigmoid(weighted_sum)
         return self.a
+
 
 class NeuralNetwork:
     def __init__(self, vision_size):
@@ -28,7 +36,7 @@ class NeuralNetwork:
         Cela sert a visualiser les cases au alentours... 
         """
         
-        self.input_size = 2 + (8 * vision_size * (vision_size - 1)) if vision_size >= 1 else 10
+        self.input_size = 4 + (8 * vision_size * (vision_size - 1)) if vision_size >= 1 else 12
         self.vision_size = vision_size
         self.depth = max(2, round(log2(max(vision_size, 2))) + 1)
         self.width = max(self.input_size + 4, 2 ** self.depth)
@@ -41,12 +49,28 @@ class NeuralNetwork:
         
         self.fitness = 0
     
-
-    def forward(self, input_values):
+    def normalize_input(inputs):
         """
-        - 2 
-        """
+        :param inputs: ## Format :
+        [loc joueur x, loc joueur y, loc ennemie x, loc ennemy, vision...]
+        Vision :
         
+        -7 trace propre
+        0 vide
+        7 mur
+        16 ennemie
+        -16 trace ennemie
+        
+        Pret défini sur un grille de 18x36
+        """
+        x = inputs[0] % 18
+        y = inputs[0] // 18
+        enemy_x = inputs[1] % 18
+        enemy_y = inputs[1] // 18
+        
+        return [x / 18, y / 36, enemy_x / 18, enemy_y / 36] + [v / 16 for v in inputs[2:]]
+
+    def forward(self, input_values):        
         assert len(input_values) == self.input_size, f"Il doit y a voir le meme nombre d'input que initier, c'esta dire la {self.input_size}"
         
         for neuron, value in zip(self.layers[0], input_values):
@@ -68,6 +92,12 @@ class NeuralNetwork:
             
         return [n.a for n in output_layer]
     
+    def predict(self):
+        return max(n.a for n in self.layers[2])
+        
+    
 
 nn = NeuralNetwork(2)
-print(nn.forward([randint(0, 5) for _ in range(18)]))
+print(f"Tilles des inputs {nn.input_size}")
+print(nn.forward([randint(-16, 16) for _ in range(nn.input_size)]))
+print(nn.predict())
