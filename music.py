eed7a700dd0c2f0785fe1470808b837136158606
Enderaction <157912904+Enderaction@users.noreply.github.com>
2025-12-15 08:48:56 +0000

    miusic

--- music.py
+++ music.py
@@ -2,5 +2,3 @@
 # Posted by The Mr. Totardo, modified by community. See post 'Timeline' for change history
 # Retrieved 2025-12-15, License - CC BY-SA 3.0
 
-import wave
-w = wave.open("e:/LOCAL/Betrayer/Metalik Klinik1-Anak Sekolah.mp3","r")
